7d23c3a3330f968a364b81c74e2142ed4c5865fb
temateorema <md318@MacBook-Pro-md318.local>
2015-01-15 00:19:47 +0200

    Initial Commit

---
+++ 34.UITableView_Navigation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  34.UITableView_Navigation
+//
+//  Created by MC723 on 15.01.15.
+//  Copyright (c) 2015 temateorema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
